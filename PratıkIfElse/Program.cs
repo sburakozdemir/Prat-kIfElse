a8a3c6a27008fa494f1b39c466fbb94d8a97363c
sburakozdemir <sburakozdemir00@gmail.com>
2024-07-17 10:46:56 +0300

    Add project files.

---
+++ PratıkIfElse/Program.cs
@@ -0,0 +1,38 @@
+﻿int sayi;
+
+Console.WriteLine("Lütfen bir sayı giriniz:");
+string StrSayi= Console.ReadLine();
+
+sayi = Convert.ToInt32(StrSayi);
+
+if (sayi == 10)
+
+{
+    Console.WriteLine("Girilen sayı 10'a eşittir.");
+}
+else if (sayi <10 )
+            
+{
+    Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+}
+
+else
+
+{
+    Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+}
+
+
+
+
+if (sayi > 0)
+
+{
+    if (sayi % 2 == 0)
+    { Console.WriteLine("Girilen sayı çifttir."); }
+    else { Console.WriteLine("Girilen Sayı tektir."); }
+}
+else
+{
+    Console.WriteLine("Girilen sayı negatif veya sıfırdır.");
+}
